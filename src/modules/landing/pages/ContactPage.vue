f7c2c77cf8c740ecf97fee020923a99f067248a8
Jose-Garrido-Dev <alejandrogarridos1992@gmail.com>
2025-01-18 01:21:05 -0300

    falta solo formulario contacto

--- src/modules/landing/pages/ContactPage.vue
+++ src/modules/landing/pages/ContactPage.vue
@@ -1,53 +1,24 @@
 <template>
   <!-- component -->
-  <section class="text-gray-600 body-font relative flex flex-1 h-full">
-    <div class="absolute inset-0 bg-gray-300">
-      <iframe
-        width="100%"
-        height="100%"
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
-        title="map"
-        scrolling="no"
-        src="https://www.google.com/maps/embed?pb=!3m2!1ses-419!2scl!4v1734806292557!5m2!1ses-419!2scl!6m8!1m7!1ss8beKorlJTE3RTXb_LXgXQ!2m2!1d-33.45828384451811!2d-70.53990172382487!3f224.1041859102053!4f-16.29007567092809!5f0.7820865974627469"
-        style=""
-      ></iframe>
-    </div>
-    <div class="container px-5 py-24 mx-auto flex">
-      <div
-        class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md"
-      >
-        <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Contactanos</h2>
-        <p class="leading-relaxed mb-5 text-gray-600">
-          Envianos un mensaje
-        </p>
-        <div class="relative mb-4">
-          <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-          />
-        </div>
-        <div class="relative mb-4">
-          <label for="message" class="leading-7 text-sm text-gray-600">mensaje</label>
-          <textarea
-            id="message"
-            name="message"
-            class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-          ></textarea>
-        </div>
-        <button
-          class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-        >
-          Button
-        </button>
-        <p class="text-xs text-gray-500 mt-3">
-          Este formulario es de prueba.
-        </p>
-      </div>
-    </div>
-  </section>
+  <section class="bg-white dark:bg-gray-900">
+  <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+      <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contáctanos</h2>
+      <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+      <form action="#" class="space-y-8">
+          <div>
+              <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+              <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required>
+          </div>
+          <div>
+              <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+              <input type="text" id="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required>
+          </div>
+          <div class="sm:col-span-2">
+              <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+              <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+          </div>
+          <button type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+      </form>
+  </div>
+</section>
 </template>
--- src/modules/landing/pages/PricingPage.vue
+++ src/modules/landing/pages/PricingPage.vue
@@ -7,7 +7,7 @@
         data-aos-once="true"
         class="my-4 text-5xl font-bold leading-tight text-blue-900"
       >
-        <span class="text-yellow-500">Galeria</span> de imágenes
+        <span class="block sm:inline ">Nuestros </span><span class="block sm:inline text-orange-400 sm:mr-20">proyectos</span> 
       </h1>
     </div>
   </section>
@@ -92,8 +92,28 @@
   </section>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
+import image1 from '@/assets/img/gallery/imagen1.jpg';
+import image2 from '@/assets/img/gallery/imagen2.jpg';
+import image3 from '@/assets/img/gallery/imagen3.jpg';
+import image4 from '@/assets/img/gallery/imagen4.jpg';
+import image5 from '@/assets/img/gallery/imagen5.jpg';
+import image6 from '@/assets/img/gallery/imagen6.jpg';
+import image7 from '@/assets/img/gallery/imagen7.jpg';
+import image8 from '@/assets/img/gallery/imagen8.jpg';
+import image9 from '@/assets/img/gallery/imagen9.jpg';
+import image10 from '@/assets/img/gallery/imagen10.jpg';
+import image11 from '@/assets/img/gallery/imagen11.jpeg';
+import image12 from '@/assets/img/gallery/imagen12.jpeg';
+import image13 from '@/assets/img/gallery/imagen13.jpeg';
+import image14 from '@/assets/img/gallery/imagen14.jpeg';
+import image15 from '@/assets/img/gallery/imagen15.jpeg';
+import image16 from '@/assets/img/gallery/imagen16.jpg';
+import image17 from '@/assets/img/gallery/imagen17.jpg';
+import image18 from '@/assets/img/gallery/imagen18.jpg';
+import image19 from '@/assets/img/gallery/imagen19.jpeg';
+import image20 from '@/assets/img/gallery/imagen20.jpeg';
 
 // Estado de la galería
 const imageGalleryOpened = ref(false);
@@ -102,16 +122,26 @@ const imageGalleryImageIndex = ref(null);
 
 // Ejemplo de imágenes (urls). Reemplaza con las tuyas.
 const images = [
-  'https://images.pexels.com/photos/2356059/pexels-photo-2356059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/3618162/pexels-photo-3618162.jpeg',
-  'https://images.unsplash.com/photo-1689217634234-38efb49cb664?ixlib=rb-4.0.3&auto=format&fit=crop&w=1887&q=80',
-  'https://images.unsplash.com/photo-1520350094754-f0fdcac35c1c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
-  'https://cdn.devdojo.com/images/june2023/mountains-10.jpeg',
-  'https://cdn.devdojo.com/images/june2023/mountains-06.jpeg',
-  'https://images.pexels.com/photos/1891234/pexels-photo-1891234.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.unsplash.com/photo-1529655683826-aba9b3e77383?ixlib=rb-4.0.3&auto=format&fit=crop&w=1965&q=80',
-  'https://images.pexels.com/photos/4256852/pexels-photo-4256852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.unsplash.com/photo-1541795083-1b160cf4f3d7?ixlib=rb-4.0.3&auto=format&fit=crop&w=1887&q=80',
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17,
+  image18,
+  image19, 
+  image20,
 ];
 
 // Mostrar el modal con la imagen
